3c700b7d7b71603b938ba391e639f9e296d01891
s41412 <s41412@bj.ac.th>
2026-01-28 11:37:18 +0700

    Add files via upload

---
+++ a4_sheep_27/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1769575012,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/point-sheet0.png",
+		"images/player-sheet0.png",
+		"images/pillar-sheet0.png",
+		"images/land-sheet0.png",
+		"images/line.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
