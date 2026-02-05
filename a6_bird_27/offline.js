d897a7780f4e0f96c68416ce07fb6046a2567aa1
s41412 <s41412@bj.ac.th>
2026-02-05 15:51:20 +0700

    Add files via upload

---
+++ a6_bird_27/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1770281523,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/blackground-sheet0.png",
+		"images/pipe_top-sheet0.png",
+		"images/pipe_borrow-sheet0.png",
+		"images/floor1.png",
+		"images/floor2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
